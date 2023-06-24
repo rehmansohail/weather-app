0d64218cd48eebe3e6a3e3ae9ea58172e227ac62
Sohail Rehman <sohailmalikraja@gmail.com>
2023-06-24 17:31:42 +0530

    added .gitignore

---
+++ src/dataModule.js
@@ -0,0 +1,48 @@
+export function processData(weatherData) {
+    // grab all the data i want to display on the page
+    const myData = {
+      condition: weatherData.current.condition.text,
+      feelsLike: {
+        f: Math.round(weatherData.current.feelslike_f),
+        c: Math.round(weatherData.current.feelslike_c),
+      },
+      currentTemp: {
+        f: Math.round(weatherData.current.temp_f),
+        c: Math.round(weatherData.current.temp_c),
+      },
+      wind: Math.round(weatherData.current.wind_mph),
+      humidity: weatherData.current.humidity,
+      location: weatherData.location.name.toUpperCase(),
+    };
+  
+    
+    myData['region'] = weatherData.location.country.toUpperCase();
+    
+  
+    return myData;
+  }
+  
+export function displayData(newData) {
+    const weatherInfo = document.getElementsByClassName('info');
+    Array.from(weatherInfo).forEach((div) => {
+      if (div.classList.contains('fade-in2')) {
+        div.classList.remove('fade-in2');
+        div.offsetWidth;
+        div.classList.add('fade-in2');
+      } else {
+        div.classList.add('fade-in2');
+      }
+    });
+    document.querySelector('.condition').textContent = newData.condition;
+    document.querySelector(
+      '.location'
+    ).textContent = `${newData.location}, ${newData.region}`;
+    document.querySelector('.degrees').textContent = newData.currentTemp.c;
+    document.querySelector(
+      '.feels-like'
+    ).textContent = `FEELS LIKE: ${newData.feelsLike.c}`;
+    document.querySelector('.wind-mph').textContent = `WIND: ${newData.wind} MPH`;
+    document.querySelector(
+      '.humidity'
+    ).textContent = `HUMIDITY: ${newData.humidity}`;
+  }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
-import './style.css';
-import background from './background.jpg';
+import { processData, displayData } from "./dataModule";
+
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('.submit-btn');
+const error = document.querySelector('.error-msg');
 
 
-console.log('sup man');
+form.addEventListener('submit', handleSubmit);
+submitBtn.addEventListener('click', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  fetchWeather();
+}
+
+async function getWeatherData(location) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=${location}`,
+    {
+      mode: 'cors',
+    }
+  );
+  if (response.status === 400) {
+    throwErrorMsg();
+  } else {
+    error.style.display = 'none';
+    const weatherData = await response.json();
+    const newData = processData(weatherData);
+    displayData(newData);
+    reset();
+  }
+}
+
+function throwErrorMsg() {
+  error.style.display = 'block';
+  if (error.classList.contains('fade-in')) {
+    error.style.display = 'none';
+    error.classList.remove('fade-in2');
+    error.offsetWidth;
+    error.classList.add('fade-in');
+    error.style.display = 'block';
+  } else {
+    error.classList.add('fade-in');
+  }
+}
+
+
+
+function reset() {
+  form.reset();
+}
+
+// get location from user
+function fetchWeather() {
+  const input = document.querySelector('input[type="text"]');
+  const userLocation = input.value;
+  getWeatherData(userLocation);
+}
+
+
+
+
