d471a1b214c7faaf517b44574ceb6b1425890864
Sohail Rehman <sohailmalikraja@gmail.com>
2023-06-28 17:06:24 +0530

    formatted documents

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { processData, displayData } from "./dataModule";
 
-const form = document.querySelector('form');
-const submitBtn = document.querySelector('.submit-btn');
-const error = document.querySelector('.error-msg');
+const form = document.querySelector("form");
+const submitBtn = document.querySelector(".submit-btn");
+const error = document.querySelector(".error-msg");
 
-
-form.addEventListener('submit', handleSubmit);
-submitBtn.addEventListener('click', handleSubmit);
+form.addEventListener("submit", handleSubmit);
+submitBtn.addEventListener("click", handleSubmit);
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -17,13 +16,13 @@ async function getWeatherData(location) {
   const response = await fetch(
     `http://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=${location}`,
     {
-      mode: 'cors',
+      mode: "cors",
     }
   );
   if (response.status === 400) {
     throwErrorMsg();
   } else {
-    error.style.display = 'none';
+    error.style.display = "none";
     const weatherData = await response.json();
     const newData = processData(weatherData);
     displayData(newData);
@@ -32,19 +31,17 @@ async function getWeatherData(location) {
 }
 
 function throwErrorMsg() {
-  error.style.display = 'block';
-  if (error.classList.contains('fade-in')) {
-    error.style.display = 'none';
-    error.classList.remove('fade-in2');
+  error.style.display = "block";
+  if (error.classList.contains("fade-in")) {
+    error.style.display = "none";
+    error.classList.remove("fade-in2");
     error.offsetWidth;
-    error.classList.add('fade-in');
-    error.style.display = 'block';
+    error.classList.add("fade-in");
+    error.style.display = "block";
   } else {
-    error.classList.add('fade-in');
+    error.classList.add("fade-in");
   }
 }
-
-
 
 function reset() {
   form.reset();
@@ -56,7 +53,3 @@ function fetchWeather() {
   const userLocation = input.value;
   getWeatherData(userLocation);
 }
-
-
-
-
